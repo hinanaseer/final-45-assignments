327c3baf9edf0a5ada432bf9567c2a2f038ee79b
hina naseer <99113701+hinanaseer@users.noreply.github.com>
2024-02-28 02:22:04 +0500

    first commit

---
+++ assignment 3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//storing the famous quote and its author
+let quote = "Nothing is impodssible because impossible itself I M POSSIBLE";
+let author = "hina Naseer";
+//printing the message
+console.log(`"${quote}" - ${author}`);
---
+++ assignment 3/main.ts
@@ -0,0 +1,9 @@
+//storing the famous quote and its author
+
+let quote:string = "Nothing is impodssible because impossible itself I M POSSIBLE"
+
+let author:string = "hina Naseer";
+
+//printing the message
+
+console.log(`"${quote}" - ${author}`);
