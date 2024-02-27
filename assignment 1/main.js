fd1de2beeb2ebab29ca71826a0885cbd36804267
hina naseer <99113701+hinanaseer@users.noreply.github.com>
2024-02-28 02:19:34 +0500

    first commit

---
+++ assignment 1/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Assidnment # 1
+// storing the person name in a variable
+let personName = "Hina";
+// Printing a message to the person
+console.log(`Hello ${personName} would you like to learn some Python today?`);
---
+++ assignment 1/main.ts
@@ -0,0 +1,9 @@
+// Assidnment # 1
+
+// storing the person name in a variable
+
+let personName:string = "Hina"
+
+// Printing a message to the person
+
+console.log(`Hello ${personName} would you like to learn some Python today?`)
